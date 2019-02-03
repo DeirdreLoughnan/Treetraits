1da4a7e071c54a6b8942d2282edb9b6b31a00e1d
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2019-02-02 23:34:05 -0800

    Improved dummy data values

--- Rcode/Proj_dummydata_Feb1.R
+++ Rcode/Proj_dummydata_Feb1.R
@@ -11,7 +11,6 @@ options(stringsAsFactors = FALSE)
 #For this early model, I will have the following variables that need estimates:
 #Traits: SLA, ht, m.dbh, wood density, C:N
 
-unique(comb)
 setwd("~/Documents/github/Treetraits")
 source('Rcode/source/Cleaning_compiling_data.R')
 
@@ -31,7 +30,7 @@ sigma<-2
 
 nsite = 2 #number of sites
 nsp = 28 #number of species
-rep = 10 # I think the maximum number is 10, but for many it is closer to 5
+rep = 20 # I think the maximum number is 10, but for many it is closer to 5
 ntot<-nsite*nsp*rep #560
 
 #Building the required dataframe for all of the replication and traits
@@ -40,19 +39,20 @@ site=gl(nsite, rep, length=ntot); site
 
 #randomly generating numbers for each trait based on what I think a small tree would be
 site.v=rnorm(ntot,5,2) #Ultimatley I will be pooling across sites, so this might not be necessary
-sla.v= rnorm(ntot, 30, 1)
-ht.v=rnorm(ntot, 10, 5) #sigma should be really high bc it includes trees and woody shrubs
-cn.v=rnorm(ntot, 30, 5)
+sla.v= rnorm(ntot, 15, 1)
+ht.v=rnorm(ntot, 7, 3) #sigma should be really high bc it includes trees and woody shrubs
+cn.v=rnorm(ntot, 10, 2)
 wood.v=rnorm(ntot, 0.5, 0.05)
-stom.v=rnorm(ntot, 400, 50)
+stom.v=rnorm(ntot, 20, 5)
 
+range(comb$cn, na.rm=T)
 #effect sizes
 site.diff=2 # I think site 2 will be delayed by 2 days due to the 5 degree diff in lat
 sla.diff= -1
-ht.diff=3
-cn.diff=-5
+ht.diff=0.5
+cn.diff=-0.5
 wood.diff=0.3
-stom.diff=25
+stom.diff=1
 
 # #sigmas
 # sla.sd=50
@@ -66,57 +66,84 @@ stom.diff=25
 mm <- model.matrix(~(site+sla.v+ht.v+cn.v+wood.v+stom.v), data.frame(site,sla.v,ht.v,cn.v,wood.v,stom.v))
 mm
 
-for (i in 1:ntot){
-  phen[i]<-int+sla.diff*sla.v[i]+rnorm(1,15, sigma)
-}
-phen
+# #stomatal density on its own
+# for (i in 1:ntot){
+#   phen[i]<-int+stom.diff*stom.v[i]+rnorm(1,15, sigma)
+# }
+# phen
+# 
+# #cn on its own
+# for (i in 1:ntot){
+#   phen[i]<-int+wood.diff*wood.v[i]+rnorm(1,15, sigma)
+# }
+# phen
+# 
+# #cn on its own
+# for (i in 1:ntot){
+#   phen[i]<-int+cn.diff*cn.v[i]+rnorm(1,15, sigma)
+# }
+# phen
+# 
+# #SLA on its own
+# for (i in 1:ntot){
+#   phen[i]<-int+sla.diff*sla.v[i]+rnorm(1,15, sigma)
+# }
+# phen
+# 
+# #ht on its own
+# for (i in 1:ntot){
+#   phen[i]<-int+ht.diff*ht.v[i]+rnorm(1,15, sigma)
+# }
+# phen
+# 
+# #Total model
+# for (i in 1:ntot){
+#   phen[i]<-int+sla.diff*sla.v[i]+ht.diff*ht.v[i]+cn.diff*cn.v[i]+wood.diff*wood.v[i]+stom.diff*stom.v[i]+rnorm(1,15, sigma)
+# }
 
-for (i in 1:ntot){
-  phen[i]<-int+sla.diff*sla.v[i]+rnorm(1,15, sigma)
-}
-phen
 
-mm.s<-scale(mm[,3:7])
-mm.s<-cbind(mm[,1:2], mm)
-head(mm.s)
+mm.cent<-scale(mm[,3:7]);mm.cent
+mm.cent<-cbind(mm[,1:2], mm.cent)
+head(mm.cent)
 
-for (i in i:mm)
-  
+coeff <- c(1, site.diff, sla.diff, ht.diff, cn.diff, wood.diff, stom.diff)
+
+phen.cent <- rnorm(n = length(site), mean = mm.cent %*% coeff, sd = 1) # This code works but the values are HUGE 
+phen.cent
+
+require(rethinking)
+simplehist(phen.cent)
+
+head(mm.cent)
+# phen
+
+fake.cent<- data.frame(phen,mm.cent)
+fake.cent
+
+summary(lm(phen ~ (site+site+sla.v+ht.v+cn.v+wood.v+stom.v), data = fake.cent)) # sanity check 
+
+####### Uncentered data #############
+coeff <- c(1, site.diff, sla.diff, ht.diff, cn.diff, wood.diff, stom.diff)
+
 phen <- rnorm(n = length(site), mean = mm %*% coeff, sd = 1) # This code works but the values are HUGE 
 phen
 
-## Here i am trying to do the same calculation a different way, but still getting gigantic values
-# phen<-vector(length=560)
- for (i in 1:ntot){
-   phen[i]<-int+sla.diff*sla.v[i]+ht.diff*ht.v[i]+cn.diff*cn.v[i]+wood.diff*wood.v[i]+stom.diff*stom.v[i]+rnorm(1,15, sigma)
- }
-phen
-
-#Figuring out why the values are so big:
-
-warnings()
-# phen
-
-
-fake <- data.frame(phen,site, sla.v,ht.v,cn.v,wood.v,stom.v)
-fake
-
-summary(lm(phen ~ (site+site+sla.v+ht.v+cn.v+wood.v+stom.v), data = fake)) # sanity check 
-
 require(rethinking)
 simplehist(phen)
+
+fake<- data.frame(phen,mm)
+fake
 
 ###### MAP MODEL ############
 an.temp<- map(
   alist(
     phen~dnorm(mu, sigma),
-    #mu<-intercept+sla.diff*sla.v+ht.diff*ht.v+cn.diff*cn.v+wood.diff*wood.v+stom.diff*stom.v,
-    mu<-intercept+stom.diff*stom.v,
+    mu<-intercept+sla.diff*sla.v+ht.diff*ht.v+cn.diff*cn.v+wood.diff*wood.v+stom.diff*stom.v,
     intercept~dnorm(20, 5),
-    #sla.v~dnorm(200, 50),
-   # ht.v~dnorm(8, 5), #sigma should be really high bc it includes trees and woody shrubs
-    #cn.v~dnorm(30, 5),
-    #wood.v~dnorm(1.5, 1),
+    sla.v~dnorm(30, 5),
+    ht.v~dnorm(8, 5), #sigma should be really high bc it includes trees and woody shrubs
+    cn.v~dnorm(30, 5),
+    wood.v~dnorm(1.5, 1),
     stom.v~dnorm(250, 50),
     sigma~dunif(5,1)
   ),
