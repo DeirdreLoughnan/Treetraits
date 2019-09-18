267086f1175453b4f35612654249ea293901a991
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2019-09-17 19:12:25 -0700

    cleaning & fake data

--- analyses/testdata/pheno_traits_na_testdata.R
+++ analyses/testdata/pheno_traits_na_testdata.R
@@ -16,7 +16,8 @@ setwd("~/Documents/github/Treetraits/Rcode/testdata")
 # <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <>
 
 nsite = 4 #number of sites with trait data
-nsp = 28 #number of species
+#nsp = 28 #number of species
+nsp = 5 #I reduced this for the purpose of building the fake data
 nind = 12
 
 rep = 1 # making it greater for the test data
@@ -49,6 +50,142 @@ treatcombo = paste(warm, photo, chill, sep = "_")
 
 d <- data.frame(treatcombo, site, warm, photo, slav)
 
+
+#### To start, keeping it simiple and ignoring interacitons between climate factors
+#### Set up differences for each level
+
+site2diff = 1 #creating a gradient, similar to what we might see with latitude 
+site3diff = 2 
+site4diff = 4  
+
+#GC climate differences
+warmdiff = -20 # days earlier from 1 to 2
+photodiff = -14
+chilldiff2 = -20 # need to think more about this number and what would be reasonable
+chilldiff3 = -10
+
+#trait differences
+sladiff = -0.5
+# htdiff=0.5
+# cndiff=-0.5
+# wooddiff=1
+
+# effects of site with warming
+site2warm = 0
+site3warm = 1
+site4warm = 2
+
+site2chill2 = 0
+site3chill2 = 1
+site4chill2 = 2
+
+site2chill3 = 1
+site3chill3 = 2
+site4chill3 = 10
+
+
+site2photo = 0
+site3photo =1
+site4photo = 5
+
+####### SD for each variable
+
+site2diff.sd = 1.5 
+site3diff.sd = 3 
+site4diff.sd = 6
+
+warmdiff.sd = 1 
+photodiff.sd = 1
+chill2diff.sd =1
+chill3diff.sd =3
+
+sladiff.sd = 1
+
+site2warm.sd = 1
+site3warm.sd = 2
+site4warm.sd = 6
+
+site2photo.sd = 1
+site3photo.sd = 1
+site4photo.sd = 1
+
+site2chill2.sd = 1
+site3chill2.sd = 1
+site4chill2.sd = 1
+site2chill3.sd = 1
+site3chill3.sd = 1
+site4chill3.sd = 1
+
+warmphoto.sd = 1
+warmchill2.sd = 1
+warmchill3.sd = 1
+
+chill2photo.sd = 1
+chill3photo.sd = 1
+
+
+
+mm<-model.matrix(~(site+warm+photo+chill)+slav, data.frame(site, warm, photo,chill,slav))
+head(mm)
+colnames(mm)
+
+fake<- vector()
+for (i in 1:nsp){
+  coeff <- c(1, site2diff,site3diff,site4diff, warmdiff, photodiff,chill2diff,chill3diff, sladiff)
+  bb <- rnorm(n = length(warm), mean = mm %*% coeff, sd = 1)
+  faket<-merge(bb,mm)
+  #faket<-data.frame(bb,mm)
+  fakex<-data.frame(faket, sp=i)
+  fake<-rbind(fake, fakex)
+}
+str(fake)
+#I think it worked! I need to think more about incorporating the trait data, right now every site has a different trait value but all GC treatments have the same value. This makes sense to me, since we might see differnces across the 4 pops, but I don't have the data to know how the traits would change with warming, chilling & photo periods.
+names(fake)
+length(fake$x);length(fake$site2)
+summary(lm(x ~ (site2+site3+site4+warm2+photo2+chill2+chill3+slav), data = fake))
+
+#########################################################################################################################
+# Adding species differences:
+baseint = 35
+spint<-baseint +c(1:nsp)-mean(1:nsp) #chaning the intercept for each indiv sp
+
+fake<-vector()
+
+for(i in 1:nsp){
+  indivint<-spint[i] +1:nind-mean(1:nind)
+  
+  coeff <- c(spint[i], 
+             rnorm(1, site2diff, site2diff.sd),
+             rnorm(1, site3diff, site3diff.sd),
+             rnorm(1, site4diff, site4diff.sd),
+             rnorm(1, warmdiff, warmdiff.sd),
+             rnorm(1, photodiff, photodiff.sd), 
+             # rnorm(1, site2warm, site2warm.sd), 
+             # rnorm(1, site2photo, site2photo.sd),
+             # rnorm(1, site3warm, site3warm.sd), 
+             # rnorm(1, site3photo, site3photo.sd),
+             # rnorm(1, site4warm, site4warm.sd), 
+             # rnorm(1, site4photo, site4photo.sd),
+             # rnorm(1, site2chill2, site2chill2.sd), 
+             # rnorm(1, site3chill2, site3chill2.sd),
+             # rnorm(1, site4chill2, site4chill2.sd),
+             # rnorm(1, site2chill3, site2chill3.sd), 
+             # rnorm(1, site3chill3, site3chill3.sd),
+             # rnorm(1, site4chill3, site4chill3.sd)
+             rnorm(1, chilldiff2, chill2diff.sd),
+             rnorm(1, chilldiff3, chill3diff.sd),
+             rnorm(1, sladiff, sladiff.sd)
+  )
+  bb <- rnorm(n = length(warm), mean = mm %*% coeff, sd = 0.1)
+  
+  fakex <- data.frame(bb, sp = i, mm)
+  
+  fake <- rbind(fake, fakex)  
+}
+
+
+#########################################################################################################################
+# Trying to make things a bit more complicated, accounting for differences in responses across sites...this might not be the direction to actually head given I only have data for 10 individuals for each population
 ###### Set up differences for each level
 sitediff =2
 sitediff2 = 2 
@@ -56,7 +193,8 @@ sitediff3 = 2
 sitediff4 = 2 
 warmdiff = -20 # days earlier from 1 to 2
 photodiff = -14
-chilldiff = -20 # need to think more about this number and what would be reasonable
+chilldiff2 = -20 # need to think more about this number and what would be reasonable
+chilldiff3 = -10
 sladiff = -0.5
 
 # htdiff=0.5
@@ -69,14 +207,27 @@ sitewarm2 = 0
 sitewarm3 = 0
 sitewarm4 = 0
 
+sitechill22 = 0
+sitechill23 = 0
+sitechill24 = 0
+sitechill32 = 0
+sitechill33 = 0
+sitechill34 = 0
+
 sitephoto = 0
 sitephoto2 = 0
 sitephoto3 = 0
 sitephoto4 = 0
 warmphoto = 3.5
-warmchill = -7 #negative, warmer days after longer chilling will advance budburst 
-chillphoto = -3.5 #negative, but smaller, longer photoperiod will result in earlier budburst, but to a lesser extent
-
+warmchill2 = -7 #negative, warmer days after longer chilling will advance budburst 
+warmchill3= -10 #additive effects of longer chilling and warm spring
+chillphoto2 = -3.5
+chillphoto3 = -1.5#negative, but smaller, longer photoperiod and greater chilling will result in earlier budburst, but to a lesser extent
+sitesla2 = 0
+sitesla3 = 0
+sitesla4= 0
+warmsla =0 
+photosla=0
 
 ####### SD for each variable
 
@@ -92,19 +243,28 @@ warmphoto.sd = 1
 warmchill.sd = 1
 chillphoto.sd = 1
 
-mm<-model.matrix(~(site+warm+photo)^2+slav, data.frame(site, warm, photo,slav))
+
+mm<-model.matrix(~(site+warm+photo+chill)^2+slav, data.frame(site, warm, photo,chill,slav))
 head(mm)
 colnames(mm)
 
 fake<- vector()
 for (i in 1:nsp){
-  coeff <- c(1, sitediff2,sitediff3,sitediff4, warmdiff, photodiff, sladiff,
-             sitewarm2,sitewarm3,sitewarm4, sitephoto2, sitephoto3,sitephoto4,
-             warmphoto)
+  coeff <- c(1, sitediff2,sitediff3,sitediff4, warmdiff, photodiff,chilldiff2,chilldiff3, sladiff,
+             sitewarm2,sitewarm3,sitewarm4, sitephoto2, sitephoto3,sitephoto4,sitechill22,sitechill23,sitechill24,
+             sitechill32,sitechill33,sitechill34,
+             warmphoto,warmchill2, warmchill3,chillphoto2,chillphoto3)
   bb <- rnorm(n = length(warm), mean = mm %*% coeff, sd = 1)
   faket<-merge(bb,mm)
+  #faket<-data.frame(bb,mm)
   fakex<-data.frame(faket, sp=i)
   fake<-rbind(fake, fakex)
 }
+str(fake)
+#I think it worked! I need to think more about incorporating the trait data, right now every site has a different trait value but all GC treatments have the same value. This makes sense to me, since we might see differnces across the 4 pops, but I don't have the data to know how the traits would change with warming, chilling & photo periods.
+length(fake$slav); length(fake$warm2); length(fake$x)
+fake$warm2
+names(fake)
+summary(lm(x~ site2+ site3 + warm2, data= fake))
 
-#I think it worked! I need to think more about incorporating the trait data, right now every site has a unique trait value. 
+names(fake)
