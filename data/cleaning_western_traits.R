cd16e126a4dd9b8dd48d98aa723ad848c4fe282b
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2022-02-04 15:05:41 -0800

    combined df and my data

--- data/cleaning_western_traits.R
+++ data/cleaning_western_traits.R
@@ -13,19 +13,19 @@
 # I think I will need two datasets: 
 # 1. Individual level metrics - height, DBH, SSD, LCC/LNN, 
 # 2. Leaf level - SLA - measured five leaves
-rm(list=ls()) 
-options(stringsAsFactors = FALSE)
+# rm(list=ls()) 
+# options(stringsAsFactors = FALSE)
 
 library(dplyr)
 library(tidyr)
 library(stringr)
 library(plyr)
 
-if(length(grep("deirdreloughnan", getwd()) > 0)) { 
-  setwd("~/Documents/github/Treetraits/data") 
-}  else{
-  setwd("/home/deirdre/treetrait") # for midge
-}
+# if(length(grep("deirdreloughnan", getwd()) > 0)) {
+#   setwd("~/Documents/github/Treetraits/data")
+# }  else{
+#   setwd("/home/deirdre/treetrait") # for midge
+# }
 
  species <- c( "acegla","betpap", "poptre", "popbal", 
            "alninc","alnvir","amealn", "corsto",
@@ -41,6 +41,7 @@ if(length(grep("deirdreloughnan", getwd()) > 0)) {
  
  sp.typ <- data.frame(species, type)
  
+ incom.smpl <- c("mp_acegla10_02","mp_loninv_4","mp_menfer_8","mp_rubpar_21","mp_rubpar_22", "mp_rubpar_7_2","sm_symalb_9_02","sm_alnvir_7", "sm_alnvir_1_June3","sm_alnvir_1", "sm_alnvir_16", "sm_betpap_7_2", "sm_poptre_10","sm_poptre_14","sm_riblac_8", "af_betpap_1","af_poptre_11","af_poptre12_02","af_rubpar_19","af_rubpar9","af_vibedu_3_02","kl_betpap_9_02") #22
  #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>#
  
  # Start with height:
@@ -48,6 +49,8 @@ ht <- read.csv("western/uncleaned/traits_ht_calc_dbh_final.csv")
 
 head(ht) 
 sort(unique(ht$species)) # has extra species
+ht$site <- as.character(ht$site)
+ht$species <- as.character(ht$species)
 ht$site[ht$site == "afrf"] <- "af"
 ht$species[ht$species == "amelan"] <- "amealn"
 ht$species[ht$species == "betpap "] <- "betpap"
@@ -71,38 +74,52 @@ ht.dbh <- ht.treeshrub[, c("species","sample","type","date","site","no","ht","db
 head(ht.dbh)
 
 length(unique(ht.dbh$sample))
+ht.dbh <- ht.dbh[!ht.dbh$sample %in% incom.smpl, ]
+length(unique(ht.dbh$sample)) # 789
 
-# ht.dbh$count <- 1
-# 
-# trt <- ht.dbh %>%
-#   group_by(site, species,) %>%
-#   summarise(no_rows = sum(count), .groups = 'drop')
+ht.dbh$count <- 1
+tempy <- aggregate(ht.dbh$count, ht.dbh[c("species","site")], FUN = sum)
 
-head(ht.dbh)
 # remove date column
 ht.dbh <- ht.dbh[, c("sample","species","site","no","type","ht","dbh","dbh2","dbh3")]
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>#
 
 #LCC/LNN 
-cn <- read.csv("western/uncleaned/Traits_cn_2019.csv")
-
-head(cn)
-sort(unique(ht.dbh$sample))
-sort(unique(cn$sample))
-
-cn$temp <- cn$sample
-cn.spp <- cn %>% separate(temp, c("site", "species","no"))
-
-sort(unique(cn.spp$species))
+# cn <- read.csv("western/uncleaned/Traits_cn_2019.csv")
+# 
+# dup <- c("af_acegla_1_2", "af_poptre_3_2","af_poptre_8_2","af_rubpar_13_2","af_rubpar_11_2","af_rubpar_10_2","af_rubpar_3_2","af_rubpar_2_2","af_rubpar_4_2","af_rubpar_5_2","af_rubpar_6_8", "af_vacmem_6_2","mp_acegla_10_2","mp_rubpar_7_2","mp_alninc_8_2","mp_spipyr_10_2","kl_riblac_2_2", "kl_riblac_10_2", "kl_loninv_8_2","kl_popbal_7_2","mp_alninc_8_2","mp_vacmem_5_2","mp_sorsco_10_2","sm_alnvir_10_2","sm_alnvir_2_2","sm_alnvir_7_ai")
+# 
+# cn <- cn[!cn$sample %in% dup, ]
+# 
+# head(cn)
+# # sort(unique(ht.dbh$sample))
+# # sort(unique(cn$sample))
+# 
+# cn$temp <- cn$sample
+# cn.spp <- cn %>% separate(temp, c("site", "species","no"))
+# 
+# sort(unique(cn.spp$species))
+# 
+# length(unique(cn.spp$sample))
+# cn.spp <- cn.spp[!cn.spp$sample %in% incom.smpl, ]
+# length(unique(cn.spp$sample)) # 800 - dups of some samples
+# 
+# cn.spp$count <- 1
+# tempy <- aggregate(cn.spp$count, cn.spp[c("species","site")], FUN = sum)
 
 # remove date column:
-cn.spp <- cn.spp[, c("sample","species","site","no","Weight..mg.","per.N","per.C","C.N")]
+cn.cl <- read.csv("western/cleaned/cn_cleaned.csv")
+dup <- c("af_acegla_1_2", "af_poptre_3_2","af_poptre_8_2","af_rubpar_13_2","af_rubpar_11_2","af_rubpar_10_2","af_rubpar_3_2","af_rubpar_2_2","af_rubpar_4_2","af_rubpar_5_2","af_rubpar_6_8", "af_vacmem_6_2","mp_acegla_10_2","mp_rubpar_7_2","mp_alninc_8_2","mp_spipyr_10_2","kl_riblac_2_2", "kl_riblac_10_2", "kl_loninv_8_2","kl_popbal_7_2","mp_alninc_8_2","mp_vacmem_5_2","mp_sorsco_10_2","sm_alnvir_10_2","sm_alnvir_2_2","sm_alnvir_7_ai","af_popbal_2_2")
+
+cn.cl <- cn.cl[!cn.cl$sample %in% dup, ]
+cn.spp <- cn.cl[, c("sample","species","site","no","Weight..mg.","per.N","per.C","C.N")]
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>#
 # Stem specific density
 
 vol <- read.csv("western/uncleaned/traits_stemvol.csv")
 wt <- read.csv("western/uncleaned/stem.wt.total.csv")
 
+vol$site <- as.character(vol$site)
 vol$site[vol$site == "afrf"] <- "af"
 
 vol.spp <- vol[vol$species %in% species, ]; sort(unique(vol.spp$species))
@@ -114,12 +131,17 @@ wt.spp$sample <- paste(wt.spp$site,wt.spp$species, wt.spp$no, sep = "_")
 ssd <- merge(vol.spp, wt.spp, by = c("species","no","site","sample"), all = TRUE)
 
 ssd$ssd <- ssd$stem.weight/ssd$vol
-ssd.temp <- ssd
-ssd.temp$count <- 1
 
-sdd.tempy <-  aggregate(ssd.temp["count"], ssd.temp[c("sample")], FUN = sum)
+length(unique(ssd$sample))
+ssd <- ssd[!ssd$sample %in% incom.smpl, ]
+length(unique(ssd$sample)) # 790
+
+ssd$count <- 1
+
+tempy <-  aggregate(ssd["count"], ssd[c("site","species")], FUN = sum)
 
 ssd <- ssd[, c("sample","species","site","no","vol","stem.weight","ssd")]
+ssd.comp <- ssd[complete.cases(ssd),]
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>#
 
 lma <- read.csv("western/uncleaned/mergedareamass_final.csv")
@@ -130,68 +152,55 @@ lma$species[lma$species == "symalb "] <- "symalb"
 lma.spp <- lma[lma$species %in% species, ]; sort(unique(lma.spp$species))
 lma.spp$sample <- paste(lma.spp$site,lma.spp$species, lma.spp$indiv.no, sep = "_")
 
+# last minute corrections:
+# lma.spp$sample <- as.character(lma.spp$sample)
+lma.spp$sample[lma.spp$sample == "kl_symalb_02_02"] <- "kl_symalb_2_02"
+lma.spp$sample[lma.spp$sample == "mp_corsto_10_2"] <- "mp_corsto_10_02"
+lma.spp$sample[lma.spp$sample == "mp_menfer_10_2"] <- "mp_menfer_10_02"
+
 lma.spp$lma <- lma.spp$leaf.mass/lma.spp$leaf.area
 
-lma.temp <-  aggregate(lma.spp["lma"], lma.spp[c("species", "site","indiv.no","sample")], FUN = mean)
-colnames(lma.temp)[colnames(lma.temp) == "indiv.no"] <- "no"
+length(unique(lma.spp$sample))
+lma.spp <- lma.spp[!lma.spp$sample %in% incom.smpl, ]
+length(unique(lma.spp$sample)) # 800 - dups of some samples
+
+# lma.spp$count <- 1
+# tempy <-  aggregate(lma.spp["count"], lma.spp[c("site","species")], FUN = sum)
+
+lma.mean <-  aggregate(lma.spp["lma"], lma.spp[c("species", "site","indiv.no","sample")], FUN = mean)
+colnames(lma.mean)[colnames(lma.mean) == "indiv.no"] <- "no"
 
 #remove collected by:
-lma.spp <- lma.spp[, c("sample","species","site","indiv.no","leaf.area","leaf.mass","lma")]
+lma.mean <- lma.mean[, c("sample","species","site","no","lma")]
+lma.comp <- lma.mean[complete.cases(lma.mean),]
+
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>#
-incom.smpl <- c("mp_acegla10_02","mp_loninv_4","mp_menfer_8","mp_rubpar_21","mp_rubpar_22", "mp_rubpar_7_2","sm_symalb_9_02","sm_alnvir_7", "sm_alnvir_1_June3","sm_alnvir_1", "sm_alnvir_16", "sm_betpap_7_2", "sm_poptre_10","sm_poptre_14","sm_riblac_8", "af_betpap_1","af_poptre_11","af_poptre12_02","af_rubpar_19","af_rubpar9","af_vibedu_3_02","kl_betpap_9_02") #24
+dim(ht.spp)
+
+# write.csv(ssd, "western/cleaned/ssd_cleaned.csv", row.names = F)
+# write.csv(cn.spp, "western/cleaned/cn_cleaned.csv", row.names = F)
+# write.csv(ht.dbh, "western/cleaned/ht_dbh_cleaned.csv", row.names = F)
+# write.csv(lma.temp, "western/cleaned/lma_cleaned.csv", row.names = F)
 
 length(unique(ssd$sample))
 length(unique(cn.spp$sample))
 length(unique(ht.dbh$sample))
 length(unique(lma.spp$sample)) # 769
-length(unique(lma.temp$sample)) # 769
+length(unique(lma.comp$sample)) # 769
 
-# Let's get a list of the samples I have complete data for:
 
-temp <- merge(ssd,cn.spp, by = c("sample", "site", "no", "species"), all =T)
-temp2 <- merge(temp, ht.dbh, by = c("sample", "site", "no", "species"), all =T)
-temp3 <- merge(temp2, lma.temp, by = c("sample", "site", "no", "species"), all =T)
+# # Let's get a list of the samples I have complete data for:
+dim(ssd)
+dim(cn.spp)
+ssd.cn <- merge(ssd,cn.spp, by = c("sample", "site", "no", "species"), all =T)
+ssd.cn.ht <- merge(ssd.cn, ht.dbh, by = c("sample", "no", "site", "species"), all =T)
+west <- merge(ssd.cn.ht, lma.comp, by = c("sample", "site", "species"), all =T)
 
-ht.temp <- temp3[complete.cases(temp3$ht),]
+# 2 rows with no trait data:
 
-ht.temp2 <- ht.temp[!ht.temp$sample %in% incom.smpl, ]
+length(unique(west$sample))
+rm <- c("af_riblac_9","sm_menfer_2")
 
-ht.temp2$count <- 1
-tempy <- aggregate(ht.temp2$count, ht.temp2[c("species","site")], FUN = sum)
+west <- west[!west$sample %in% rm, ]
 
-write.csv(ht.temp, "temp_traitdata.csv", row.names = F)
-# smp <- unique(ssd$sample)
-# 
-# t <- lma.spp[!lma.spp$sample %in% smp, ]
-# sort(unique(t$sample))
-# 
-# lma.spp$count <- 1
-# lma.spp$site <- as.factor(lma.spp$site)
-# lma.spp$species <- as.factor(lma.spp$species)
-# 
-# aggregate(lma.spp$count, lma.spp[c("species", "site")], FUN = sum)
-
-###########################################
-# subset the other traits to match the cleaned leaf data:
-
-smp <- unique(lma.spp$sample)
-
-ssd.smp <- ssd[ssd$sample %in% smp, ]
-length(unique(ssd.smp$sample))
-
-ssd.smp$count <- 1
-temp <- aggregate(ssd.smp$count, ssd.smp[c("sample")], FUN = sum)
-
-#####
-cn.smp <- cn.spp[cn.spp$sample %in% smp, ]
-length(unique(cn.smp$sample))
-
-cn.smp$count <- 1
-temp <- aggregate(cn.smp$count, cn.smp[c("sample")], FUN = sum)
-
-#####
-ht.dbh.smp <- ht.dbh[ht.dbh$sample %in% smp, ]
-length(unique(ht.dbh.smp$sample))
-
-ht.dbh.smp$count <- 1
-temp <- aggregate(ht.dbh.smp$count, ht.dbh.smp[c("sample")], FUN = sum)
+#write.csv(west, "western/cleaned/westTrait.csv", row.names = F)
---
+++ data/combiningEastWest.R
@@ -0,0 +1,34 @@
+# Started Feb 4, 2022
+
+# Aim of this code is to combine the eastern and western trait data into a single file
+rm(list=ls()) 
+options(stringsAsFactors = FALSE)
+
+setwd("~/Documents/github/Treetraits/data")
+
+source("cleaning_western_traits.R")
+
+source("cleaning_eastern_traits.R")
+
+head(trt)
+
+head(west)
+#Start by changing column names in the datasets so they are least match
+colnames(trt)[colnames(trt)=="Individual"] <- "sample"
+colnames(trt)[colnames(trt)=="Site"] <- "site"
+colnames(trt)[colnames(trt)=="Species"] <- "species"
+colnames(trt)[colnames(trt)=="X.N"] <- "per.N"
+colnames(trt)[colnames(trt)=="X.C"] <- "per.C"
+colnames(trt)[colnames(trt)=="Height"] <- "ht"
+colnames(trt)[colnames(trt)=="DBH"] <- "dbh"
+colnames(trt)[colnames(trt)=="DBH.2"] <- "dbh2"
+colnames(trt)[colnames(trt)=="DBH.3"] <- "dbh3"
+colnames(trt)[colnames(trt)=="Stomatal.Density"] <- "stomatal.density"
+
+#Many of the columns are character, but should be numeric with NA
+trt.sub <- trt[,c("sample","site","species","ssd","per.N","per.C","ht","dbh","dbh2","dbh3","lma")]
+
+west.sub <- west[, c("sample","site","species","type","ssd","per.N","per.C","C.N","ht","dbh","dbh2","dbh3","lma")]
+
+allTrt <- rbind.fill(trt.sub, west.sub)
+#write.csv(allTrt, "allTrt.csv", row.names = F)
