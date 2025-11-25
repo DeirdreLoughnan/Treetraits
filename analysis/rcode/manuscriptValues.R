3f71a4208539a6c15846bdfdc37200f8baecc193
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2025-11-24 22:28:26 -0800

    edits to ms values

--- analysis/rcode/manuscriptValues.R
+++ analysis/rcode/manuscriptValues.R
@@ -136,6 +136,9 @@ muSp <- data.frame(htModelFit$mu_grand_sp)
 muSpMean <- colMeans(muSp)
 htFold <- -1*round(max(muSpMean)/min(muSpMean),0)
 
+htLat<- as.numeric(round(mean(htModelFit$b_lat),1))
+
+
 htTran <- as.numeric(round(mean(htModelFit$b_tranE),1))
 lower_htTran <- format(round(quantile(htModelFit$b_tranE, prob = 0.05),1), nsmall =1)
 upper_htTran <- round(quantile(htModelFit$b_tranE, prob = 0.95),1)
@@ -232,6 +235,9 @@ lmaMuPSpMean; lower_lmaMuPSpMean; upper_lmaMuPSpMean
 lmachill <- data.frame(lmaModelFit$alphaChillSp)
 lmaChillSpMean <- colMeans(lmachill)
 lmaChillMax <- max(lmaChillSpMean)
+
+lmaLat <- as.numeric(round(mean(lmaModelFit$b_lat),1))
+
 
 lmaLatTran <- as.numeric(round(mean(lmaModelFit$b_tranlat),1))
 lower_lmaLatTran <- format(round(quantile(lmaModelFit$b_tranlat, prob = 0.05),1), nsmall =1)
@@ -354,6 +360,8 @@ ssdchill <- data.frame(ssdModelFit$alphaChillSp)
 ssdChillSpMean <- colMeans(ssdchill)
 ssdChillMax <- max(ssdChillSpMean)
 
+ssdLat <- as.numeric(round(mean(ssdModelFit$b_lat),1))
+
 ssdLatTran <- as.numeric(round(mean(ssdModelFit$b_tranlat),1))
 lower_ssdLatTran <- format(round(quantile(ssdModelFit$b_tranlat, prob = 0.05),1), nsmall =1)
 upper_ssdLatTran <- round(quantile(ssdModelFit$b_tranlat, prob = 0.95),1)
@@ -425,3 +433,181 @@ totalObs <- nrow(below)
 
 
 totalDays <- max(tot$day)
+
+#######################################
+sumHt <- summary(mdlHt)$summary
+postHt <- rstan::extract(mdlHt)
+
+a_sp = (sumHt[grep("mu_grand_sp", rownames(sumHt)), 1])
+b_photo = sumHt[grep("betaPhotoSp\\[", rownames(sumHt)), 1]
+b_chill = sumHt[grep("betaChillSp\\[", rownames(sumHt)), 1]
+b_force = sumHt[grep("betaForceSp\\[", rownames(sumHt)), 1]
+
+a_sp5 <- vector()
+for(i in 1:ncol(postHt$mu_grand_sp)){
+  quantU <- round(quantile(postHt$mu_grand_sp[,i], c(0.05, 0.95, 0.25, 0.75)),1)
+  a_sp5 <- rbind(a_sp5, quantU)
+}
+colnames(a_sp5) <- c("Int5","Int95","Int25","Int75")
+#a_sp5 <- a_sp5/100
+
+b_chill5 <- vector()
+for(i in 1:ncol(postHt$betaChillSp)){
+  quantU <- round(quantile(postHt$betaChillSp[,i], c(0.05, 0.95, 0.25, 0.75)),1)
+  b_chill5 <- rbind(b_chill5, quantU)
+}
+colnames(b_chill5) <- c("chill5","chill95","chill25","chill75")
+
+b_force5 <- vector()
+for(i in 1:ncol(postHt$betaForceSp)){
+  quantU <- round(quantile(postHt$betaForceSp[,i], c(0.05, 0.95, 0.25, 0.75)),1)
+  b_force5 <- rbind(b_force5, quantU)
+}
+colnames(b_force5) <- c("force5","force95","force25","force75")
+
+b_photo5 <- vector()
+for(i in 1:ncol(postHt$betaPhotoSp)){
+  quantU <- round(quantile(postHt$betaPhotoSp[,i], c(0.05, 0.95, 0.25, 0.75)),1)
+  b_photo5 <- rbind(b_photo5, quantU)
+}
+colnames(b_photo5) <- c("photo5","photo95","photo25","photo75")
+
+# #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>#
+
+#If we are using the old model, we will use the z-scored values for the parameters
+photo <- -0.5033863 #8 h photo
+force <- -0.3568628 #5/15 C trt
+chill <- -0.3546922 # low chill
+
+m <- matrix(nrow = 1000, ncol = 47)
+
+for(sp in 1:47){
+  for (it in 1:nrow(m)){
+    m[it,sp] <- postHt$mu_grand_sp[it,sp]+  
+      postHt$betaForceSp[it,sp] * force + 
+      postHt$betaPhotoSp[it, sp] * photo + 
+      postHt$betaChillSp[it,sp] * chill 
+  }
+}
+
+photoHigh <- 0.4965051 #8 h photo
+siteSM <- 0
+forceHigh <- 0.5877121 
+chillHigh <- 0.3660412 # high chill for Smithers
+
+mHigh <- matrix(nrow = 1000, ncol = 47)
+
+for(sp in 1:47){
+  for (it in 1:nrow(mHigh)){
+    mHigh[it,sp] <- postHt$mu_grand_sp[it,sp]+  
+      postHt$betaForceSp[it,sp] * forceHigh + 
+      postHt$betaPhotoSp[it, sp] * photoHigh + 
+      postHt$betaChillSp[it,sp] * chillHigh 
+  }
+}
+
+spInfo <- spInfo[order(spInfo$species),]
+spInfo$meanBB <- colMeans(m)
+colnames(m) <- spInfo$species.name
+
+spInfo$meanBBHigh <- colMeans(mHigh)
+colnames(mHigh) <- spInfo$species.name
+
+spInfo$Int <- a_sp
+spInfo <- cbind(spInfo, a_sp5,b_force5, b_chill5,b_photo5)
+
+spInfo$force <- b_force
+spInfo$chill <- b_chill
+spInfo$photo <- b_photo
+
+quantile595 <- function(x){
+  returnQuanilte <- quantile(x, prob = c(0.05, 0.95, 0.25,0.75))
+  return(returnQuanilte)
+}
+
+bb_quan <- apply(m, 2, quantile595)
+bb_t <- t(bb_quan)
+bb_df <- data.frame(bb_t)
+colnames(bb_df)[colnames(bb_df) == "X5."] <- "bb5"
+colnames(bb_df)[colnames(bb_df) == "X95."] <- "bb95"
+colnames(bb_df)[colnames(bb_df) == "X25."] <- "bb25"
+colnames(bb_df)[colnames(bb_df) == "X75."] <- "bb75"
+
+
+bb_quanHigh <- apply(mHigh, 2, quantile595)
+bb_tHigh <- t(bb_quanHigh)
+bb_dfHigh <- data.frame(bb_tHigh)
+colnames(bb_dfHigh)[colnames(bb_dfHigh) == "X5."] <- "bb5High"
+colnames(bb_dfHigh)[colnames(bb_dfHigh) == "X95."] <- "bb95High"
+colnames(bb_dfHigh)[colnames(bb_dfHigh) == "X25."] <- "bb25High"
+colnames(bb_dfHigh)[colnames(bb_dfHigh) == "X75."] <- "bb75High"
+
+spInfo <- cbind(spInfo, bb_df)
+spInfo$value <- spInfo$meanBB
+
+spInfo <- cbind(spInfo, bb_dfHigh)
+spInfo$valueHigh <- spInfo$meanBBHigh
+
+m <- data.frame(m)
+
+long <- reshape2::melt(m)
+names(long) <- c("species.name", "valueLow")
+ 
+mHigh <- data.frame(mHigh)
+
+longHigh <- reshape2::melt(mHigh)
+names(longHigh) <- c("species.name", "valueHigh")
+
+long <- cbind(long, longHigh[,2])
+
+long <- merge(long,spInfo, by = "species.name")
+
+spOrderData <- spInfo[order(spInfo$meanBB),]
+spOrder <- as.factor(spOrderData$species.name)
+# 
+# spOrderHtData <- spInfo[order(spInfo$ht),]
+# spOrderHt <- as.factor(spOrderData$species.name)
+# 
+# long <- long[order(long$species),]
+
+# longPhotoInfo$mean <- rowMeans(longPhotoInfo[,c("Site1","Site2","Site3","Site4")], na.rm=TRUE)
+
+bChill <- data.frame(postHt$betaChillSp[1:1000,])
+colnames(bChill) <- (spInfo$species.name)
+longChill <- reshape2::melt(bChill)
+names(longChill) <- c("species.name", "chill")
+
+long <- cbind(long, longChill$chill)
+
+# Add forcing
+bForce <- data.frame(postHt$betaForceSp[1:1000,])
+colnames(bForce) <- (spInfo$species.name)
+longForce <- reshape2::melt(bForce)
+names(longForce) <- c("species.name", "force")
+
+long <- cbind(long, longForce$force)
+
+# photoperiod
+bPhoto <- data.frame(postHt$betaPhotoSp[1:1000,])
+colnames(bPhoto) <- (spInfo$species.name)
+longPhoto <- reshape2::melt(bPhoto)
+names(longPhoto) <- c("species.name", "photo")
+
+long <- cbind(long, longPhoto$photo)
+
+# intercept
+aSp <- data.frame(postHt$mu_grand_sp[1:1000,])
+colnames(aSp) <- (spInfo$species.name)
+longInt <- reshape2::melt(aSp)
+names(longInt) <- c("species.name", "int")
+
+long <- cbind(long, longInt$int)
+
+data <- long[order(long$meanBB),]
+
+shrub <- subset(data, type == "shrub")
+meanShrubBB <- round(mean(shrub$meanBB), 1)
+
+tree <- subset(data, type == "tree")
+meanTreeBB <- round(mean(tree$meanBB), 1)
+
